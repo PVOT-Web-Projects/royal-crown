2df318ba87b2c3988813ea7228cdd1c9de743d51
meetchauhandweck <meet.chauhan@dweckenterprise.com>
2024-06-10 11:59:31 +0530

    head

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -7,40 +7,15 @@ const Header = () => {
       <h1>Header</h1>
       <nav>
         <ul>
-          <li>
-            <Link href="/" legacyBehavior>
-              <a>Home</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/about-us" legacyBehavior>
-              <a>About Us</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/catalogue" legacyBehavior>
-              <a>Catalogue</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/laminate-library" legacyBehavior>
-              <a>Laminate Library</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/products" legacyBehavior>
-              <a>Products</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/whats-new" legacyBehavior>
-              <a>Whats New</a>
-            </Link>
-          </li>
+          <li><Link href="/" legacyBehavior>Home</Link></li>
+          <li><Link href={"/about-us"} legacyBehavior>About Us</Link></li>
+          <li><Link href={"/catalogue"} legacyBehavior>catalogue</Link></li>
+          <li><Link href={"/laminate-library"} legacyBehavior>Laminate Library</Link></li>
+          <li><Link href={"/products"} legacyBehavior>Products</Link></li>
+          <li><Link href={"/whats-new"} legacyBehavior>Whats New</Link></li>
         </ul>
       </nav>
     </>
   );
 };
-
 export default Header;
