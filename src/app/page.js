04a83de9615a57be60b07dbe7826f1bb70034298
meetchauhandweck <meet.chauhan@dweckenterprise.com>
2024-06-10 13:58:14 +0530

    banner and nav -m

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,9 @@
+import HeroBanner from "@/components/HeroBanner/HeroBanner";
 
 export default function Home() {
   return (
     <main className="main">
-     <h1>Home Page</h1>
+     <HeroBanner />
     </main>
   );
 }
