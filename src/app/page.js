7e9bc167e21354937e804d2848a43b8ebb58542c
meetchauhandweck <meet.chauhan@dweckenterprise.com>
2024-06-11 19:32:01 +0530

    fd

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Headingwithbutton from "@/components/Headingwithbutton/headingwithbutton"
 import Form from "@/components/form/Form";
 import SocialPage from "@/components/socialPage/socialpage";
 import SocialMediaImg from "@/images/socials-image.png";
-import Insightslider from "@/components/InsightSlider/insightslider1";
+// import Insightslider from "@/components/InsightSlider/insightslider1";
 import "./Home.scss";
 
 export default function Home() {
@@ -12,7 +12,7 @@ export default function Home() {
       <HeroBanner />
       <Headingwithbutton />
 
-      <Insightslider />
+      {/* <Insightslider /> */}
       <div className="Form_Page">
         <SocialPage socialMediaImgSrc={SocialMediaImg} />
         <Form />
