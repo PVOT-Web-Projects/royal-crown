3cb136ac0d0bbffbfb4a73b2a657d9a0a210c2f1
Sapana <jaiswalsapna080@gmail.com>
2024-06-11 11:30:17 +0530

    changessss

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,15 @@
 import HeroBanner from "@/components/HeroBanner/HeroBanner";
 import Headingwithbutton from "@/components/Headingwithbutton/headingwithbutton";
+import Certification from "@/components/Certifications/Certification";
+import Insightslider from "@/components/Insightslider/Insightslider";
 
 export default function Home() {
   return (
     <main className="main">
       <HeroBanner />
       <Headingwithbutton />
+      <Certification/>
+      <Insightslider/>
     </main>
   );
 }
