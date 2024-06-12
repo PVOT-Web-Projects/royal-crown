fde13303e20a3d61da188b71915ba1a947495c31
Sapana <jaiswalsapna080@gmail.com>
2024-06-12 10:24:48 +0530

    changesss

--- src/app/page.js
+++ src/app/page.js
@@ -4,12 +4,14 @@ import Form from "@/components/form/Form";
 import SocialPage from "@/components/socialPage/socialpage";
 import SocialMediaImg from "@/images/socials-image.png";
 import "./variables.scss";
+import CircularScroll from "@/components/Circularscroll/Circularscroll";
 
 export default function Home() {
   return (
     <main className="main">
       <HeroBanner />
       <Headingwithbutton />
+      <CircularScroll/>
     </main>
   );
 }
