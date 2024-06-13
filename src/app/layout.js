d486f552b62043ae3e724e8e51cba3c2bbc20b08
meetchauhandweck <meet.chauhan@dweckenterprise.com>
2024-06-13 19:27:49 +0530

    gg

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-import Headerss from "@/components/header/Header";
+// import Header from "@/components/header/Header";
 import "./globals.scss";
 import Footer from "@/components/footer/Footer";
 
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head></head>
       <body>
-        <Headerss />
+        {/* <Header /> */}
         {children}
         <Footer />
       </body>
