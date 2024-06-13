7973b2935a378825874a9e34bd374ef8c1b8c0cc
meetchauhandweck <meet.chauhan@dweckenterprise.com>
2024-06-13 19:40:13 +0530

    gg

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-"use client";
+// "use client";
 
 import Link from "next/link";
 import logo from "@/images/svgLogos/header_logo.svg";
@@ -6,14 +6,14 @@ import logo2 from "@/images/svgLogos/header_crown_logo.svg";
 import "./header.scss";
 import Image from "next/image";
 import LinkHover from "../linkHover/LinkHover";
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 
 const Header = () => {
   return (
     <header>
       <nav>
         <ul>
-          <motion.li
+          <li
             initial={{
               opacity: 0,
             }}
@@ -26,8 +26,8 @@ const Header = () => {
             viewport={{ once: true }}
           >
             <LinkHover url={"#"} text={"About Us"} fontSize={"16px"} />
-          </motion.li>
-          <motion.li
+          </li>
+          <li
             initial={{
               opacity: 0,
             }}
@@ -40,8 +40,8 @@ const Header = () => {
             viewport={{ once: true }}
           >
             <LinkHover url={"#"} text={"Products"} fontSize={"16px"} />
-          </motion.li>
-          <motion.li
+          </li>
+          <li
             initial={{
               opacity: 0,
             }}
@@ -54,9 +54,9 @@ const Header = () => {
             viewport={{ once: true }}
           >
             <LinkHover url={"#"} text={"What's New"} fontSize={"16px"} />
-          </motion.li>
+          </li>
         </ul>
-        <motion.div
+        <div
           className="logo"
           initial={{
             opacity: 0,
@@ -72,9 +72,9 @@ const Header = () => {
           <Link href={"/"}>
             <Image src={logo} alt="header_logo" />
           </Link>
-        </motion.div>
+        </div>
         <ul>
-          <motion.li
+          <li
             initial={{
               opacity: 0,
             }}
@@ -87,8 +87,8 @@ const Header = () => {
             viewport={{ once: true }}
           >
             <LinkHover url={"#"} text={"Catalogue"} fontSize={"16px"} />
-          </motion.li>
-          <motion.li
+          </li>
+          <li
             initial={{
               opacity: 0,
             }}
@@ -101,7 +101,7 @@ const Header = () => {
             viewport={{ once: true }}
           >
             <LinkHover url={"#"} text={"Laminate Library"} fontSize={"16px"} />
-          </motion.li>
+          </li>
           <div className="side_logo">
             <Image src={logo2} alt="header_crown_logo" />
           </div>
