b0d488bf2b9cea0a471284a5447f89c85ecc0c70
meetchauhandweck <meet.chauhan@dweckenterprise.com>
2024-06-13 11:37:24 +0530

    shadow -m

--- src/components/shadowHeading/ShadowHeading.jsx
+++ src/components/shadowHeading/ShadowHeading.jsx
@@ -1,10 +1,20 @@
 import "./shadowHeading.scss";
 
-const ShadowHeading = ({ text }) => {
+const ShadowHeading = ({ text, fontSize }) => {
   return (
     <div className="shadow_heading">
-      <div className="shadow_heading_text shadow">{text}</div>
-      <div className="shadow_heading_text normal">{text}</div>
+      <div
+        className="shadow_heading_text shadow"
+        style={{ fontSize: fontSize }}
+      >
+        {text}
+      </div>
+      <div
+        className="shadow_heading_text normal"
+        style={{ fontSize: fontSize }}
+      >
+        {text}
+      </div>
     </div>
   );
 };
