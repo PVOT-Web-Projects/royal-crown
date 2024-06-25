8f5d9f06bc945e56c2bf5d3d65accd79931f5168
utkarshyadav11 <utkarshyadav6745@gmail.com>
2024-06-25 17:50:42 +0530

    new push

--- src/app/about-us/page.jsx
+++ src/app/about-us/page.jsx
@@ -2,11 +2,13 @@ import Form from "@/components/form/Form";
 import SocialPage from "@/components/socialPage/socialpage";
 import SocialMediaImg from "@/images/socials-image.png";
 import Aboutus_vision from "@/components/Aboutus_vision/page"
+import Aboutus_sizes from "@/components/Aboutus_sizes/page"
 import "./Home.scss";
 const AboutUs = () => {
   return (
     <div className="abous_us">
     <Aboutus_vision/>
+    <Aboutus_sizes/>
      
       <div className="Form_Page">
         <SocialPage socialMediaImgSrc={SocialMediaImg} />
