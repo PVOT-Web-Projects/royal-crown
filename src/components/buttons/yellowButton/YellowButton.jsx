d8348767148e2cd2d94ac864127ee40729b0c173
meetchauhandweck <meet.chauhan@dweckenterprise.com>
2024-07-04 18:56:17 +0530

    category page -m

--- src/components/buttons/yellowButton/YellowButton.jsx
+++ src/components/buttons/yellowButton/YellowButton.jsx
@@ -3,8 +3,8 @@ import "./yellowButton.scss";
 
 const YellowButton = ({ url, btn_text }) => {
   return (
-    <Link href={url} class="yello_btn">
-      <span class="button-content">{btn_text} </span>
+    <Link href={url} className="yello_btn">
+      <span className="button-content">{btn_text} </span>
     </Link>
   );
 };
