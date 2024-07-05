9f374ac90224469c5670f52d604bbe96b986b80f
utkarshyadav11 <utkarshyadav6745@gmail.com>
2024-07-05 19:02:30 +0530

    new push

--- src/components/aboutUs_products/page.jsx
+++ src/components/aboutUs_products/page.jsx
@@ -4,28 +4,122 @@ import "./aboutUs_product.scss";
 import Image from "next/image";
 import products from "./productData.js";
 import { Dropdown } from "primereact/dropdown";
+
+
 const Page = () => {
   const [selectedBrand, setSelectedBrand] = useState("all");
   const [selectedType, setSelectedType] = useState("all");
   const [selectedCategory, setSelectedCategory] = useState("all");
+  const[selectedFinish,setSelectedFinish]= useState("all");
+  const[selectedSize,setSelectedSize]= useState("all");
+  const[selectedThickness,setSelectedThickness]= useState("all");
+  const[selectedColor,setSelectedColor]= useState("all");
 
-  const handleBrandChange = (event) => {
-    setSelectedBrand(event.target.value);
+  const brands = [
+    { label: "All Brands", value: "all" },
+    { label: "Xylem", value: "Xylem" },
+    { label: "Royal Crown", value: "Royal Crown" },
+    { label: "QBliss", value: "QBliss" },
+    { label: "Crown XCL", value: "Crown XCL" },
+  ];
+
+  const categories = [
+    { label: "All Categories", value: "all" },
+    { label: "Decorative", value: "Decorative" },
+    { label: "Interior Compacts", value: "Decorative Interior Compacts" },
+    { label: "Exterior Compacts", value: "Decorative Exterior Compacts" },
+  ];
+
+  const types = [
+    { label: "All Types", value: "all" },
+    { label: "Spotless", value: "Spotless" },
+    { label: "Exotic Urbane", value: "Exotic Urbane" },
+    { label: "Classic Wood Grains", value: "Classic Wood Grains" },
+    { label: "Stones", value: "Stones" },
+    { label: "Solid Colors", value: "Solid Colors" },
+    { label: "Textiles", value: "Textiles" },
+    { label: "Mirrors", value: "Mirrors" },
+    { label: "Woodgrains", value: "Woodgrains" },
+  ];
+
+  const finish = [
+    { label: "Royal Art", value: "Royal Art" },
+    { label: "Finish1", value: "Finish1" },
+    { label: "Finish2", value: "Finish2" },
+    { label: "Finish3", value: "Finish3" },
+  ];
+
+  const size = [
+    { label: "8 x 2", value: "8 x 2" },
+    { label: "8 x 9", value: "8 x 9" },
+    { label: "3 x 2", value: "3 x 2" },
+    { label: "6 x 6", value: "6 x 6" },
+  ];
+
+  const thickness = [
+    { label: "0.9 MM", value: "0.9 MM" },
+    { label: "0.4 MM", value: "0.4 MM" },
+    { label: "1.2 MM", value: "1.2 MM" },
+    { label: "2.5 MM", value: "2.5 MM" },
+  ];
+
+  const color = [
+    { label: "Red", value: "Red" },
+    { label: "Blue", value: "Blue" },
+    { label: "Green", value: "Green" },
+    { label: "Gray", value: "Gray" },
+    { label: "Brown", value: "Brown" },
+    { label: "Pink", value: "Pink" },
+    { label: "Yellow", value: "Yellow" },
+    { label: "White", value: "White" },
+  ];
+
+ 
+
+
+  const handleBrandChange = (e) => {
+    setSelectedBrand(e.value);
   };
 
-  const handleTypeChange = (event) => {
-    setSelectedType(event.target.value);
+  const handleTypeChange = (e) => {
+    setSelectedType(e.value);
   };
 
-  const handleCategoryChange = (event) => {
-    setSelectedCategory(event.target.value);
+  const handleCategoryChange = (e) => {
+    setSelectedCategory(e.value);
   };
+
+
+  const handleFinishChange = (e) => {
+    setSelectedFinish(e.value);
+  };
+
+  const handleSizeChange = (e) => {
+    setSelectedSize(e.value);
+  };
+
+  const handleThicknessChange = (e)=>{
+    setSelectedThickness(e.value);
+  };
+
+  const handleColorChange = (e)=>{
+    setSelectedColor(e.value);
+  };
+
+
+
+
+
 
   const filteredProducts = products.filter((product) => {
     const brandMatch = selectedBrand === "all" || product.category === selectedBrand;
     const typeMatch = selectedType === "all" || product.categoryType === selectedType;
+    const colorMatch = selectedColor === "all" || product.categoryColor === selectedColor;
     const categoryMatch = selectedCategory === "all" || product.categoryValue === selectedCategory;
-    return brandMatch && typeMatch && categoryMatch;
+    const finishMatch = selectedFinish === "all" || product.categoryFinish === selectedFinish;
+    const sizeMatch = selectedSize === "all" || product.categorySize === selectedSize;
+    const thicknessMatch = selectedThickness === "all" || product.categoryThickness === selectedThickness;
+    return brandMatch && typeMatch && categoryMatch;  // add  new dropdown data to get proper result
   });
 
   return (
@@ -39,43 +133,101 @@ const Page = () => {
               <label htmlFor="brand-select" className="dropdown-label">
                 SELECT BRAND
               </label>
-              <select id="brand-select" className="category-select" onChange={handleBrandChange}>
-                <option className="option" value="all">All Brands</option>
-                <option className="option" value="Xylem">Xylem</option>
-                <option className="option" value="Royal Crown">Royal Crown</option>
-                <option className="option" value="QBliss">QBliss</option>
-                <option className="option" value="Crown XCL">Crown XCL</option>
-              </select>
+              <Dropdown
+                id="brand-select"
+                options={brands}
+                value={selectedBrand}
+                onChange={handleBrandChange}
+                placeholder="Select a Brand"
+                className="category-select"
+              />
             </div>
 
             <div className="dropdown1">
               <label htmlFor="category-select" className="dropdown-label">
                 SELECT CATEGORY
               </label>
-              <select id="category-select" className="category-select" onChange={handleCategoryChange}>
-                <option className="option" value="all">All Categories</option>
-                <option className="option" value="Decorative">Decorative</option>
-                <option className="option" value="Decorative Interior Compacts">Interior Compacts</option>
-                <option className="option" value="Decorative Exterior Compacts">Exterior Compacts</option>
-              </select>
+              <Dropdown
+                id="category-select"
+                options={categories}
+                value={selectedCategory}
+                onChange={handleCategoryChange}
+                placeholder="Select a Category"
+                className="category-select"
+              />
             </div>
 
             <div className="dropdown1">
               <label htmlFor="type-select" className="dropdown-label">
                 SELECT TYPE
               </label>
-              <select id="type-select" className="category-select" onChange={handleTypeChange}>
-                <option className="option" value="all">All Types</option>
-                <option className="option" value="Spotless">Spotless</option>
-                <option className="option" value="Exotic Urbane">Exotic Urbane</option>
-                <option className="option" value="Classic Wood Grains">Classic Wood Grains</option>
-                <option className="option" value="Stones">Stones</option>
-                <option className="option" value="Solid Colors">Solid Colors</option>
-                <option className="option" value="Textiles">Textiles</option>
-                <option className="option" value="Mirrors">Mirrors</option>
-                <option className="option" value="Woodgrains">Woodgrains</option>
-              </select>
+              <Dropdown
+                id="type-select"
+                options={types}
+                value={selectedType}
+                onChange={handleTypeChange}
+                placeholder="Select a Type"
+                className="category-select"
+              />
             </div>
+
+            <div className="dropdown1">
+              <label htmlFor="color-select" className="dropdown-label">
+                SELECT COLOR
+              </label>
+              <Dropdown
+                id="color-select"
+                options={color}
+                value={selectedColor}
+                onChange={handleColorChange}
+                placeholder="Select a Color"
+                className="color-select"
+              />
+            </div>
+
+
+            <div className="dropdown1">
+              <label htmlFor="finish-select" className="dropdown-label">
+                SELECT FINISH
+              </label>
+              <Dropdown
+                id="type-select"
+                options={finish}
+                value={selectedFinish}
+                onChange={handleFinishChange}
+                placeholder="Select a Finish"
+                className="category-select"
+              />
+            </div>
+
+            <div className="dropdown1">
+              <label htmlFor="finish-select" className="dropdown-label">
+                SELECT FINISH
+              </label>
+              <Dropdown
+                id="size-select"
+                options={size}
+                value={selectedSize}
+                onChange={handleSizeChange}
+                placeholder="Select a Size"
+                className="category-select"
+              />
+            </div>
+
+            <div className="dropdown1">
+              <label htmlFor="finish-select" className="dropdown-label">
+                SELECT THICKNESS
+              </label>
+              <Dropdown
+                id="thickness-select"
+                options={thickness}
+                value={selectedThickness}
+                onChange={handleThicknessChange}
+                placeholder="Select a Thickness"
+                className="category-select"
+              />
+            </div>
+
           </div>
 
           <div className="container">
