e6822979a262c8de3bbcea79dd6ed30b6ff1ef97
jeel1802 <160886657+jeel1802@users.noreply.github.com>
2024-07-11 16:20:03 +0530

    commit

--- src/components/aboutUs_Establishment/aboutUs_Establish.jsx
+++ src/components/aboutUs_Establishment/aboutUs_Establish.jsx
@@ -1,58 +1,17 @@
 "use client";
 import "./aboutUs_Establishment.scss";
 import Image from "next/image";
-import timeLine from "../../images/aboutUs_timeline_image.png";
+import timeLine from "../../images/laminate - about-us 2.png";
 import icon from "../../images/crown11.png";
 import { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 const AboutUsEstablishment = () => {
-  const yearsRef = useRef([null]);
-  const sectionRef = useRef(null);
-  gsap.registerPlugin(ScrollTrigger);
-
-  useEffect(() => {
-    const updateFontSize = () => {
-      const isLargeScreen = window.matchMedia("(min-width: 768px)").matches;
-      const largeSize = isLargeScreen ? "4rem" : "2rem";
-      const smallSize = isLargeScreen ? "2rem" : "1rem";
-
-      yearsRef.current.forEach((year, index) => {
-        ScrollTrigger.create({
-          trigger: year,
-          start: "top center",
-          end: "bottom center",
-          // markers : true,
-          // pin : true,
-          onEnter: () => {
-            gsap.to(year, { color: "black", fontSize: largeSize, scale: 1.5, duration: 0.5 });
-            yearsRef.current.forEach((otherYear, otherIndex) => {
-              if (otherIndex !== index) {
-                gsap.to(otherYear, { color: "grey", fontSize: smallSize, scale: 1, duration: 0.5 });
-              }
-            });
-          },
-          onLeaveBack: () => gsap.to(year, { color: "grey", fontSize: smallSize, scale: 1, duration: 0.5 }),
-          onEnterBack: () => gsap.to(year, { color: "black", fontSize: largeSize, scale: 1.5, duration: 0.5 }),
-          onLeave: () => gsap.to(year, { color: "grey", fontSize: smallSize, scale: 1, duration: 0.5 }),
-        });
-      });
-    };
-
-    updateFontSize();
-    window.addEventListener("resize", updateFontSize);
-
-    return () => {
-      window.removeEventListener("resize", updateFontSize);
-      ScrollTrigger.getAll().forEach(trigger => trigger.kill());
-    };
-  }, []);
-
   return (
     <div className="about_Establishment">
       <div className="Establishment_Wrapper" >
-        <div className="left" ref={sectionRef}>
+        <div className="left">
           <div className="icon">
             <Image src={icon} alt="Crown Icon" />
           </div>
@@ -63,19 +22,16 @@ const AboutUsEstablishment = () => {
           <div className="box">
             <p
               className="year"
-              ref={(el) => (yearsRef.current[0] = el)}
             >
               1990
             </p>
             <p
               className="year other-years"
-              ref={(el) => (yearsRef.current[1] = el)}
             >
               1993
             </p>
             <p
               className="year other-years"
-              ref={(el) => (yearsRef.current[2] = el)}
             >
               1993
             </p>
