0fe792ac4128343b54c4a165016508c51e42fe74
jeel1802 <160886657+jeel1802@users.noreply.github.com>
2024-07-11 15:00:02 +0530

    about us responsive

--- src/components/aboutLaminate/AboutLaminate.jsx
+++ src/components/aboutLaminate/AboutLaminate.jsx
@@ -1,17 +1,22 @@
 import Image from "next/image";
 import "./aboutLaminate.scss";
-import crown from '../../images/crown11.png';
+import crown from "../../images/crown11.png";
 
 const AboutLaminate = ({ heading, text, leftText, rightText, image }) => {
   return (
     <div className="about_laminate">
       <div className="wrapper">
-      <div className="right">
-          <div className="right_aboutus">ABOUT <br/> US</div>
+        <div className="right">
+          <div className="right_aboutus">
+            ABOUT <br /> US
+          </div>
           <div className="right_image">
             <Image src={image} alt={heading} />
           </div>
-          <div className="scroll_down">SCROLL <br/>DOWN</div>
+          <div className="scroll_down">
+            SCROLL <br />
+            DOWN
+          </div>
         </div>
         <div className="left">
           <div className="heading_text">
@@ -20,11 +25,13 @@ const AboutLaminate = ({ heading, text, leftText, rightText, image }) => {
             <div className="text">{text}</div>
           </div>
           <div className="bottom_text">
-            <Image src={crown} alt="crown"/>
+            <Image src={crown} alt="crown" />
             <div className="right_text">{rightText}</div>
           </div>
         </div>
       </div>
+        <div className="scrollDown">  SCROLL <br />
+        DOWN</div>
     </div>
   );
 };
