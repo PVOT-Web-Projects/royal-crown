c785c7142a538d03ba5cdd2b30d766d6fa283d69
Jay Kalariya <93344658+Jay-Kalariya@users.noreply.github.com>
2024-07-22 19:08:08 +0530

    new upd -j

--- src/app/careers/page.jsx
+++ src/app/careers/page.jsx
@@ -1,9 +1,13 @@
+import CareerGetStarted from "@/components/career_get_started/page";
 import CareerJob from "@/components/career_job/page";
+import CareerForm from "@/components/forms/careerform/page";
 
 const Career =() =>{
     return(
         <div>
-            {/* <CareerJob /> */}
+            <CareerJob />
+            <CareerGetStarted />
+           <CareerForm />
         </div>
     )
 }
